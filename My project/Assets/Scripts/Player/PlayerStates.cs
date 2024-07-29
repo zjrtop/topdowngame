0bb1a41407186e8ac0ce75a9c7f3daa63ca9bdeb
yao li <78089386+ELFyao@users.noreply.github.com>
2024-07-29 15:40:07 +0800

    1、修复光环bug，UI调整

--- My project/Assets/Scripts/Player/PlayerStates.cs
+++ My project/Assets/Scripts/Player/PlayerStates.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class PlayerStates : MonoBehaviour
 {
@@ -10,14 +11,50 @@ public class PlayerStates : MonoBehaviour
 
     public int progress = 0;
 
+    public Sprite sprite;
 
 
     public void IncrementShield() {
+
+
+
+        // 在父对象中查找名为"C"的子对象
+        Transform cTransform = transform.Find("UpperBuffer");
+
+        if (cTransform != null)
+        {
+            // 获取子对象C的GameObject
+            GameObject cGameObject = cTransform.gameObject;
+            Debug.Log("Found child GameObject C: " + cGameObject.name);
+            SpriteRenderer spriteRenderer = cGameObject.GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                Debug.Log("Found MyComponent in ChildObject.");
+                spriteRenderer.sprite = sprite;
+            }
+            
+        }
         detectTimes =  1;
     }
 
     public void DecreaseShield() {
         detectTimes = 0;
+        Transform cTransform = transform.Find("UpperBuffer");
+
+        if (cTransform != null)
+        {
+            // 获取子对象C的GameObject
+            GameObject cGameObject = cTransform.gameObject;
+            Debug.Log("Found child GameObject C: " + cGameObject.name);
+            SpriteRenderer spriteRenderer = cGameObject.GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                Debug.Log("Found MyComponent in ChildObject.");
+                spriteRenderer.sprite = null;
+            }
+
+        }
+        detectTimes = 1;
     }
 
     public int GetDetectTimes() {
