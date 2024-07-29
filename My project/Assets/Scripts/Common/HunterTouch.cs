8e8116880a69cf7689c5a3e216f0b26a515a09e8
Yihua (Anthony) Wang <141053175+AnthonyWang19@users.noreply.github.com>
2024-07-29 04:52:05 -0400

    道具

--- My project/Assets/Scripts/Common/HunterTouch.cs
+++ My project/Assets/Scripts/Common/HunterTouch.cs
@@ -5,29 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class HunterTouch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 检查的 PlayerPrefs 键
+    public string key1 = "myIntKey";
+    public string key2 = "buttonClickStatus";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // 目标场景名称
+    public string scene1 = "HouseScene2";
+    public string scene2 = "HouseScene3";
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        // ��ȡ�������Transform
         GameObject colliderObj = other.gameObject;
-
         if (colliderObj.CompareTag("humanbody"))
         {
-            PlayerStates playerStates = colliderObj.GetComponentInParent<PlayerStates>();
-            int progress = playerStates.progress;
-            if (progress == 2)
-                SceneManager.LoadScene("HouseScene3");
+            // 获取两个 PlayerPrefs 的值
+            int value1 = PlayerPrefs.GetInt(key1, 0);
+            int value2 = PlayerPrefs.GetInt(key2, 0);
+
+            // 检查两个值是否都为2
+            if (value1 == 2 && value2 == 2)
+            {
+                SceneManager.LoadScene(scene2);
+            }
+            else
+            {
+                SceneManager.LoadScene(scene1);
+            }
         }
     }
-
 }
